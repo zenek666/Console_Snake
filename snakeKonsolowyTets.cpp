d3467dce92aa278eabc021b1fe1fae86147ca2e8
kamil20020 <kamdyw@wp.pl>
2021-03-18 21:39:43 +0100

    Wydaje się, że wszystko zostało zaimplementowane.

--- snakeKonsolowyTets.cpp
+++ snakeKonsolowyTets.cpp
@@ -1,11 +1,28 @@
-// snakeKonsolowyTets.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
 
 using namespace std;
+
+int* bodyX = new int[5];
+int* bodyY = new int[5];
+
+int sizeBody = 5;
+
+int fieldX = 50;
+int fieldY = 20;
+
+int foodX = 30;
+int foodY = 15;
+
+int points = 0;
+
+bool doEnd = false;
+
+int direction = 0;
+
+int* prevBodyX = new int[5];
+int* prevBodyY = new int[5];
 
 void gotoxy(int x, int y) {
 
@@ -17,7 +34,7 @@ void gotoxy(int x, int y) {
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
-void clear(int fieldX, int fieldY) {
+void clear() {
 
     for (int i = 1; i < fieldX-2; i++) {
 
@@ -29,7 +46,7 @@ void clear(int fieldX, int fieldY) {
     }
 }
 
-void clearSnake(int* bodyX, int* bodyY, int sizeBody) {
+void clearSnake() {
 
     for (int i = 0; i < sizeBody; i++) {
 
@@ -38,7 +55,7 @@ void clearSnake(int* bodyX, int* bodyY, int sizeBody) {
     }
 }
 
-void drawFrame(int fieldX, int fieldY) {
+void drawFrame() {
 
     for (int j = 1; j < fieldX; j++) {
 
@@ -65,7 +82,7 @@ void drawFrame(int fieldX, int fieldY) {
     }
 }
 
-void drawSnake(int* bodyX, int* bodyY, int sizeBody) {
+void drawSnake() {
 
     for (int i = 0; i < sizeBody; i++) {
 
@@ -74,16 +91,217 @@ void drawSnake(int* bodyX, int* bodyY, int sizeBody) {
     }
 }
 
-void drawFood(int foodX, int foodY) {
+void drawFood() {
 
     gotoxy(foodX, foodY);
     cout << "$";
 }
 
-void drawPoints(int points, int x, int y) {
+void drawPoints(int x, int y) {
 
     gotoxy(x, y);
     cout << "Punkty: " << points;
+}
+
+void keyUse() {
+
+    if (_kbhit()) {
+
+        switch (_getch()) {
+
+        case 27:
+
+            doEnd = true;
+            break;
+
+        case 119: //W
+
+            if (direction != 2)
+                direction = 1;
+            break;
+
+        case 115: //S
+
+            if (direction != 1)
+                direction = 2;
+            break;
+
+        case 97: //A
+
+            if (direction != 4)
+                direction = 3;
+            break;
+
+        case 100: //D
+
+            if (direction != 3)
+                direction = 4;
+            break;
+        }
+    }
+}
+
+void moving() {
+
+    switch (direction) {
+
+        case 1:
+
+            if (bodyY[0] > 1) {
+
+                bodyY[0]--;
+
+                for (int i = 1; i < sizeBody; i++) {
+
+                    bodyX[i] = prevBodyX[i - 1];
+                    bodyY[i] = prevBodyY[i - 1];
+                }
+            }
+            else
+                doEnd = true;
+
+            break;
+
+        case 2:
+
+            if (bodyY[0] < fieldY) {
+
+                bodyY[0]++;
+
+                for (int i = 1; i < sizeBody; i++) {
+
+                    bodyX[i] = prevBodyX[i - 1];
+                    bodyY[i] = prevBodyY[i - 1];
+                }
+            }
+            else
+                doEnd = true;
+
+            break;
+
+        case 3:
+
+            if (bodyX[0] > 1) {
+
+                bodyX[0]--;
+
+                for (int i = 1; i < sizeBody; i++) {
+
+                    bodyX[i] = prevBodyX[i - 1];
+                    bodyY[i] = prevBodyY[i - 1];
+                }
+            }
+            else
+                doEnd = true;
+
+            break;
+
+        case 4:
+
+            if (bodyX[0] < fieldX - 3) {
+
+                bodyX[0]++;
+
+                for (int i = 1; i < sizeBody; i++) {
+
+                    bodyX[i] = prevBodyX[i - 1];
+                    bodyY[i] = prevBodyY[i - 1];
+                }
+            }
+            else
+                doEnd = true;
+
+            break;
+    }
+}
+
+void incrementBody() {
+
+    sizeBody++;
+
+    int* newBodyX = new int[sizeBody];
+    int* newBodyY = new int[sizeBody];
+
+    for (int i = 0; i < sizeBody - 1; i++) {
+
+        newBodyX[i] = bodyX[i];
+        newBodyY[i] = bodyY[i];
+    }
+
+    newBodyX[sizeBody - 1] = 0;
+    newBodyY[sizeBody - 1] = 0;
+
+    delete[] bodyX;
+    delete[] bodyY;
+
+    bodyX = newBodyX;
+    bodyY = newBodyY;
+
+    prevBodyX = new int[sizeBody];
+    prevBodyY = new int[sizeBody];
+
+    switch (direction) {
+
+        case 1:
+
+            bodyX[sizeBody - 1] = bodyX[sizeBody - 2];
+            bodyY[sizeBody - 1] = 3;
+
+            break;
+
+        case 2:
+
+            bodyX[sizeBody - 1] = bodyX[sizeBody - 2];
+            bodyY[sizeBody - 1] = bodyY[sizeBody - 2] - 1;
+
+            break;
+
+        case 3:
+
+            bodyX[sizeBody - 1] = bodyX[sizeBody - 2] + 1;
+            bodyY[sizeBody - 1] = bodyY[sizeBody - 2];
+
+            break;
+
+        case 4:
+
+            bodyX[sizeBody - 1] = bodyX[sizeBody - 2] - 1;
+            bodyY[sizeBody - 1] = bodyY[sizeBody - 2];
+
+            break;
+    }
+}
+
+void ateFoodLogic() {
+
+    incrementBody();
+
+    while (true) {
+
+        bool fieldIsClear = false;
+
+        foodX = rand() % fieldX - 3 + 1;
+        foodY = rand() % fieldY + 1;
+
+        for (int i = 0; i < sizeBody; i++) {
+
+            if (bodyX[i] == foodX && bodyY[i] == foodY) {
+
+                fieldIsClear = false;
+                break;
+            }
+            else
+                fieldIsClear = true;
+        }
+
+        if (fieldIsClear)
+            break;
+    }
+
+    drawFood();
+    points++;
+
+    drawPoints(60, 10);
 }
 
 int main()
@@ -96,32 +314,21 @@ int main()
 
     SetConsoleCursorInfo(myconsole, &cursor);
 
-    int* bodyX = new int[1];
-    int* bodyY = new int[1];
-
     bodyX[0] = 10;
     bodyY[0] = 10;
+    bodyX[1] = 9;
+    bodyY[1] = 10;
+    bodyX[2] = 8;
+    bodyY[2] = 10;
+    bodyX[3] = 7;
+    bodyY[3] = 10;
+    bodyX[4] = 6;
+    bodyY[4] = 10;
 
-    int sizeBody = 1;
-
-    int fieldX = 50;
-    int fieldY = 20;
-
-    int foodX = 30;
-    int foodY = 15;
-    
-    bool doEnd = false;
-
-    int direction = 0;
-
-    int points = 0;
-
-    drawFrame(fieldX, fieldY);
-    drawSnake(bodyX, bodyY, sizeBody);
-    drawFood(foodX, foodY);
-    drawPoints(points, 60, 10);
-
-    //dostepna powiezchnia do -3 od szerokosci i - 1 wysokosci
+    drawFrame();
+    drawSnake();
+    drawFood();
+    drawPoints(60, 10);
 
     while (!doEnd) {
 
@@ -129,115 +336,38 @@ int main()
 
         Sleep(100);
 
-        if (_kbhit()) {
-
-            switch (_getch()) {
-
-            case 27:
-
-                doEnd = true;
-                break;
-
-            case 119: //W
-
-                direction = 1;
-                break;
-
-            case 115: //S
-
-                direction = 2;
-                break;
-
-            case 97: //A
-
-                direction = 3;
-                break;
-
-            case 100: //D
-
-                direction = 4;
-                break;
-            }
-        }
-
-        clearSnake(bodyX, bodyY, sizeBody);
-
-        switch (direction) {
-
-            case 1:
-
-                for (int i = 0; i < sizeBody; i++) {
-
-                    if (bodyY[i] > 1)
-                        bodyY[i]--;
-                }
-
-            break;
-
-            case 2:
-
-                for (int i = 0; i < sizeBody; i++) {
-
-                    if (bodyY[i] < fieldY)
-                        bodyY[i]++;
-                }
-
-            break;
-
-            case 3:
-
-                for (int i = 0; i < sizeBody; i++) {
-
-                    if (bodyX[i] > 1)
-                        bodyX[i]--;
-                }
-
-            break;
-
-            case 4:
-
-                for (int i = 0; i < sizeBody; i++) {
-
-                    if (bodyX[i] < fieldX - 3)
-                        bodyX[i]++;
-                }
-
-            break;
-        }
+        keyUse();
 
         if (direction > 0) {
 
-            if (bodyX[0] == foodX && bodyY[0] == foodY) {
+            clearSnake();
 
-                while (true) {
+            for (int i = 0; i < sizeBody; i++) {
 
-                    bool fieldIsClear = false;
+                prevBodyX[i] = bodyX[i];
+                prevBodyY[i] = bodyY[i];
+            }
+        }
 
-                    foodX = rand() % fieldX - 3 + 1;
-                    foodY = rand() % fieldY + 1;
+        moving();
 
-                    for (int i = 0; i < sizeBody; i++) {
+        if (direction > 0) {
 
-                        if (bodyX[i] == foodX && bodyY[i] == foodY) {
+            if (bodyX[0] == foodX && bodyY[0] == foodY)
+                ateFoodLogic();
+            else {
 
-                            fieldIsClear = false;
-                            break;
-                        }
-                        else
-                            fieldIsClear = true;
-                    }
+                for (int i = 1; i < sizeBody; i++) {
 
-                    if (fieldIsClear)
+                    if (bodyX[0] == bodyX[i] && bodyY[0] == bodyY[i]) {
+
+                        doEnd = true;
                         break;
+                    }
                 }
-
-                drawFood(foodX, foodY);
-                points++;
-
-                drawPoints(points, 60, 10);
             }
 
-            drawSnake(bodyX, bodyY, sizeBody);
+            drawSnake();
         }
     }
 }
